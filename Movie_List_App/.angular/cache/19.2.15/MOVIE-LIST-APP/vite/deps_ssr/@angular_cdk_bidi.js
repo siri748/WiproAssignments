fca456faaa284a6de008b2e3cc92b503e617e3ae
Siri <julianrajumatte@gmail.com>
2025-08-29 12:10:11 +0530

    message

---
+++ Movie_List_App/.angular/cache/19.2.15/MOVIE-LIST-APP/vite/deps_ssr/@angular_cdk_bidi.js
@@ -0,0 +1,16 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-PFGQHEKB.js";
+import "./chunk-YT24NAFA.js";
+import "./chunk-PXXRCHXC.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
